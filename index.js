6aae6464fb8098681a60c14ef0f34643f17a8fa7
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-02-17 09:51:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: "https://quizmasterss.netlify.app/",
+  origin: "https://quizmasterss.netlify.app",
   credentials: true
 }))   
 
